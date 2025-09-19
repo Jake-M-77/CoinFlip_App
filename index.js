1889e824fc0441fae0a351c060b8529d046f47ff
Jake M <jakemanson77@icloud.com>
2025-09-19 16:49:53 +0100

    feat: add score box, update styling, and improve coin flip JS
    - Added a score box displaying total head count, total tail count, and previous flip result
    - Styled the score box and h1 tags using a Tudor-era font (UnifrakturCook)
    - Updated existing styles: page background, coin size, flip button styling and hover/active states
    - Refactored JS:
      - Constants for DOM elements in caps
      - Updated coin flip logic to track head/tail counts and previous flip
      - Improved readability and maintainability of AnimateCoin, UpdateCoinFace, and CoinRandomiser functions

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-const coinFlipBtn = document.getElementById("flipBtn");
+const COIN_FLIP_BTN = document.getElementById("flipBtn");
 let coinImage = document.getElementById("coin");
+const HEAD_COUNT_TEXT = document.getElementById("headCount");
+const TAIL_COUNT_TEXT = document.getElementById("tailCount");
+const PREVIOUS_FLIP_TEXT = document.getElementById("previousFlip");
+
 let isHead = true;
 let spinAmount = 5;
 let spinCounter = 0;
 
-coinFlipBtn.addEventListener("click", event => {
-    console.log("Test");
-    
+let headCount = 0;
+let tailCount = 0;
+let previousFlip = "";
+
+
+
+
+COIN_FLIP_BTN.addEventListener("click", event => {
+
     FlipTimes(spinAmount);
     spinCounter = 0;
-
+    if (tailCount || headCount > 0) {
+        PREVIOUS_FLIP_TEXT.textContent = previousFlip;
+    }
 });
 
-function Sleep(ms){
+function Sleep(ms) {
     return new Promise(Resolve => setTimeout(Resolve, ms));
 }
 
-async function FlipTimes(spinAmount){
+async function FlipTimes(spinAmount) {
     for (let i = 0; i < spinAmount; i++) {
         AnimateCoin();
         await Sleep(COINSPINDURATION.duration);
@@ -42,14 +54,12 @@ async function AnimateCoin() {
 
 function UpdateCoinFace() {
     spinCounter++;
-    if(isHead)
-    {
+    if (isHead) {
         coinImage.src = "/images/coinTail.jpg";
     }
-    else
-    {
+    else {
         coinImage.src = "/images/coinFace.jpg";
-        
+
     }
 
     CoinRandomiser();
@@ -57,16 +67,19 @@ function UpdateCoinFace() {
     isHead = !isHead;
 }
 
-function CoinRandomiser(){
+function CoinRandomiser() {
 
     let random = Math.floor(Math.random() * 2);
     console.log(random);
 
-    if(spinCounter === spinAmount)
-    {
-        random === 1 ? coinImage.src="/images/coinTail.jpg" : coinImage.src="/images/coinFace.jpg";
+    if (spinCounter === spinAmount) {
+        random === 1 ? coinImage.src = "/images/coinTail.jpg" : coinImage.src = "/images/coinFace.jpg";
+        random === 1 ? tailCount++ : headCount++;
+        random === 1 ? previousFlip = "Tails" : previousFlip = "Heads";
+        HEAD_COUNT_TEXT.textContent = headCount;
+        TAIL_COUNT_TEXT.textContent = tailCount;
     }
 }
 
-    
+
 
